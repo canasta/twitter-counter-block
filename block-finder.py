89e61a8bde71f1612a386b37ab0d90cbd1f13918
canasta <canasta102@gmail.com>
2017-11-06 20:37:27 +0900

    Add files via upload

---
+++ block-finder.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+import tweepy
+from tweepy import OAuthHandler
+
+auth = tweepy.OAuthHandler("Consumer Key (API Key)", "Consumer Secret (API Secret)") #YOU MUST FIX HERE
+auth.set_access_token("Access Token", "Access Token Secret") #YOU MUST FIX HERE
+
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True, retry_count=10, retry_delay=5, retry_errors=5)
+
+blockcount = 0
+depthlimit = 3 # Resize value as you want
+
+def findblock(id, depth):
+	count = 0
+	try:
+		user = api.get_user(id)
+	except tweepy.TweepError as e:
+		print(str(e))
+		time.sleep(10)
+		return 0
+	except TypeError:
+		return 0
+	except Exception as e:
+		print(str(e))
+		return 0
+	if user.protected == True: #protected
+		return 0
+	elif user.statuses_count == 0: #No tweets
+		return 0
+	elif not hasattr(user,"status"): #blocked
+		try:
+			print("Try to block:", user.screen_name, "(ID:", id, ")")
+			api.create_block(id)
+		except Exception:
+			print("Failed:", user.screen_name, "(ID:", id, ")")
+			return 0
+	else: #normal
+		nextlist = api.friends_ids(id)
+		for next in nextlist:
+			if depth<depthlimit:
+				res = findblock(next, depth+1)
+				count = count + res
+		return count
+
+if __name__ == "__main__": 
+	me = api.me()
+	followings = api.friends_ids(me.screen_name)
+	for friend in followings:
+		list = api.friends_ids(friend)
+		for user in list:
+			res = findblock(user, 1)
+			blockcount = blockcount + res
+	print("Block ", blockcount, " accounts")
