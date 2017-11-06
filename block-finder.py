55df5e4d5160a5b5a5653e8183ae5e7306e9c74b
canasta <canasta102@gmail.com>
2017-11-06 20:53:23 +0900

    Update block-finder.py
    Add the list of ID that already checked to reduce request

--- block-finder.py
+++ block-finder.py
@@ -8,9 +8,14 @@ auth.set_access_token("Access Token", "Access Token Secret") #YOU MUST FIX HERE
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True, retry_count=10, retry_delay=5, retry_errors=5)
 
 blockcount = 0
-depthlimit = 3 # Resize value as you want
+depthlimit = 3 #resize as you want
+checked = {}
 
 def findblock(id, depth):
+	if id in checked:
+		return 0
+	else:
+		checked.append(id)
 	count = 0
 	try:
 		user = api.get_user(id)
@@ -47,6 +52,8 @@ if __name__ == "__main__":
 	followings = api.friends_ids(me.screen_name)
 	for friend in followings:
 		list = api.friends_ids(friend)
+		print(len(list))
+		sys.exit()
 		for user in list:
 			res = findblock(user, 1)
 			blockcount = blockcount + res
