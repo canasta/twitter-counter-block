4f74ddbee73016290310855a42f2ab768c0c2bbb
canasta <canasta102@gmail.com>
2017-11-13 15:32:56 +0900

    Update block-finder.py

--- block-finder.py
+++ block-finder.py
@@ -2,50 +2,68 @@
 import tweepy
 from tweepy import OAuthHandler
 
-auth = tweepy.OAuthHandler("Consumer Key (API Key)", "Consumer Secret (API Secret)") #YOU MUST FIX HERE
-auth.set_access_token("Access Token", "Access Token Secret") #YOU MUST FIX HERE
+auth = tweepy.OAuthHandler("Consumer Key (API Key)", "Consumer Secret (API Secret)")
+auth.set_access_token("Access Token", "Access Token Secret")
 
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True, retry_count=10, retry_delay=5, retry_errors=5)
 
 blockcount = 0
-depthlimit = 5 #resize as you want
+depthlimit = 4 #resize as you want
 checked = []
-
+try:
+	with open("./checked.list","r+") as list:
+		checked = list.read().split()
+except:
+	with open("./checked.list","w") as list:
+		checked = []
+	
+		
 def findblock(id, depth):
 	if id in checked:
 		return 0
-	else:
-		checked.append(id)
+			
 	count = 0
 	try:
 		user = api.get_user(id)
 	except tweepy.TweepError as e:
 		print(str(e))
-		time.sleep(10)
 		return 0
 	except TypeError:
 		return 0
 	except Exception as e:
 		print(str(e))
 		return 0
+		
 	if user.protected == True: #protected
 		return 0
-	elif user.statuses_count == 0: #No tweets
+	elif user.statuses_count == 0: #No tweets(Dummy account)
 		return 0
 	elif not hasattr(user,"status"): #blocked
 		try:
-			print("Try to block:", user.screen_name, "(ID:", id, ")")
 			api.create_block(id)
-			return 1
+			blockcount = blockcount + 1
+			print("(",blockcount,")Block:", user.screen_name, "(ID:", id, ")")
+			with open("./blocked.list", "a") as list:
+				list.write("%s %s\n"%(user.screen_name,id))
 		except Exception:
 			print("Failed:", user.screen_name, "(ID:", id, ")")
-			return 0
+			return -1
 	else: #normal
-		nextlist = api.friends_ids(id)
-		for next in nextlist:
-			if depth<depthlimit:
-				count = count + findblock(next, depth+1)
-		return count
+		if depth<depthlimit:
+			nextlist = api.friends_ids(id)
+			if len(nextlist)>20 and len(nextlist)<500:
+				for next in nextlist:
+					findblock(next, depth+1)
+			else:
+				with open("./skipped.list","a") as list:
+					list.write("%s %s\n"%(user.screen_name,id))
+				print("- Skipped:", user.screen_name, "(ID:", id, ")")
+				
+	checked.append(id)
+	with open("./checked.list", "a") as list:
+		list.write("%s %s\n"%(user.screen_name,id))
+		
+	return 0
 
 if __name__ == "__main__": 
 	me = api.me()
@@ -53,5 +71,5 @@ if __name__ == "__main__":
 	for friend in followings:
 		list = api.friends_ids(friend)
 		for user in list:
-			blockcount = blockcount + findblock(user, 1)
+			findblock(user, 1)
 	print("Block ", blockcount, " accounts")
